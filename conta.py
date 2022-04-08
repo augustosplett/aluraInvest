83a26405568d68704774cfbb86fd04aa802bcab2
Augusto Madeira Splett (Tinho) <augusto.splett@gmail.com>
2022-04-07 21:01:30 -0300

    commit final

--- conta.py
+++ conta.py
@@ -1,20 +1,42 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        self.__numero__ = numero
-        self.__titular__ = titular
-        self.__saldo__ = saldo
-        self.__limite__ = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
-    def obter_saldo(self):
-        return "o saldo em conta é de R${}".format(self.__saldo__)
+    def get_extrato(self):
+        return "o saldo em conta é de R${}".format(self.__saldo)
+
+    def get_titular(self):
+        return self.__titular
 
     def depositar(self, valor_deposito):
-        self.__saldo__ += valor_deposito
+        self.__saldo += valor_deposito
+
+    def __pode_sacar(self, valor):
+        valor_disponivel = self.__saldo + self.__limite
+        return valor <= valor_disponivel
 
     def sacar(self, valor_saque):
-        self.__saldo__ -= valor_saque
+        if(self.__pode_sacar(valor_saque)):
+            self.__saldo -= valor_saque
+        else:
+            print("saldo insuficiente")
 
     def transferir(self, valor, conta_destino):
         self.sacar(valor)
         conta_destino.depositar(valor)
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, valor):
+        self.__limite = valor
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
